e34387feb6013f845bc6ce63dd75725393e834bf
wn789 <768187171@qq.com>
2018-12-19 19:42:33 +0800

    Update unixbench.sh

--- unixbench.sh
+++ unixbench.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
-#==============================================================#
-#   Description:  Unixbench script                             #
-#   Author: Teddysun <i@teddysun.com>                          #
-#   Intro:  https://teddysun.com/245.html                      #
-#==============================================================#
+
 cur_dir=/opt/unixbench
 
 # Check System
@@ -30,7 +26,7 @@ if [ -s UnixBench5.1.3.tgz ]; then
     echo "UnixBench5.1.3.tgz [found]"
 else
     echo "UnixBench5.1.3.tgz not found!!!download now..."
-    if ! wget -c https://dl.lamp.sh/files/UnixBench5.1.3.tgz; then
+    if ! wget -c https://github.com/wn789/unixbench/raw/master/UnixBench5.1.3.tgz; then
         echo "Failed to download UnixBench5.1.3.tgz, please download it to ${cur_dir} directory manually and try again."
         exit 1
     fi
